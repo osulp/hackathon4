0b772e948efda75453f4ba480b0b7a38f0e5e190
Josh Gum <revgum@gmail.com>
2019-05-03 09:18:53 -0700

    cas authenticate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :cas_authenticatable
+
+  def cas_extra_attributes=(extra_attributes)
+    extra_attributes.each do |name, value|
+      case name.to_sym
+      when :email
+        self.email = value
+      end
+    end
+  end
 end
