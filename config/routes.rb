d3679d205debf7ea8b83222124d91d2bf0964a11
Josh Gum <revgum@gmail.com>
2019-05-03 09:10:32 -0700

    authenticated route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    cas_sessions: 'my_cas'
-  }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root to: 'home#index', as: :authenticated_root
+    end
+  end
 end
