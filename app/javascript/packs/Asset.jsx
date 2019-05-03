4240da3711588ae2455e08520e459e60e46f3035
Fischer Jemison <jemisonf@oregonstate.edu>
2019-05-03 13:43:25 -0700

    semifinal frontend code with stub api

--- app/javascript/packs/Asset.jsx
+++ app/javascript/packs/Asset.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import Child from './Child';
 
 class Asset extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props.children);
+    this.handleShowPeople = this.handleShowPeople.bind(this);
+  }
+
+  handleShowPeople() {
+    this.props.loadPeople(this.props.parent.id);
+  }
+
   render() {
     return (
-      <div class="asset">
-        <div class="header">
-          <h2>{this.parent.name}</h2>
+      <div className="asset">
+        <div className="header">
+          <h2>{this.props.parent.name}</h2>
         </div>
-        <div class="body">
+        <div className="body">
           <p>Related items:</p>
-          <div class="children">
+          <div className="children">
             {
-              this.children.forEach(child => {
-                <Child name={child.name} type={child.type} />
-              })
+              this.props.children.length > 0 ? this.props.children.map(child => {
+                return <Child key={child.id} name={child.name} type={child.type} />
+              }) : ""
             }
           </div>
+          <a href="#" onClick={this.handleShowPeople}>Show people with this skill</a>
         </div>
       </div>
     );
